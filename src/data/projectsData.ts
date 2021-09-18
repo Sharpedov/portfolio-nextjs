687ebd9375810608e289ef3da34acac6c6d0460f
Adrian <adr654231@gmail.com>
2021-09-18 22:00:33 +0200

    add another project

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -2,6 +2,26 @@ export const projectsData = [
 	{
 		id: 1,
 		framework: "Next.js",
+		title: "Posts",
+		description: {
+			pl: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula tellus, lacinia ut ligula in, vulputate cursus est. Nunc sed urna ut mauris sagittis pulvinar vel ac elit.",
+			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula tellus, lacinia ut ligula in, vulputate cursus est. Nunc sed urna ut mauris sagittis pulvinar vel ac elit.",
+		},
+		images: [
+			"/projects/project_2/Screenshot_1.png",
+			"/projects/project_2/Screenshot_2.png",
+			"/projects/project_2/Screenshot_3.png",
+			"/projects/project_2/Screenshot_5.png",
+			"/projects/project_2/Screenshot_6.png",
+			"/projects/project_2/Screenshot_7.png",
+			"/projects/project_2/Screenshot_8.png",
+		],
+		href: "https://posts-nextjs-lemon.vercel.app/",
+		source: "https://github.com/Sharpedov/posts-nextjs",
+	},
+	{
+		id: 2,
+		framework: "Next.js",
 		title: "Eshop ecommerce",
 		description: {
 			pl: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula tellus, lacinia ut ligula in, vulputate cursus est. Nunc sed urna ut mauris sagittis pulvinar vel ac elit.",
